78a8db524b06392c22b95cafdd0cfadc4d082d0c
Qian Gong <12826359+qgong-its@users.noreply.github.com>
2026-06-09 12:18:21 +0200

    feat(log): implement logger service (#3)
    * feat(log): resolve and create logs directory
    
    * feat(log): generate operation log file name
    
    * feat(log): append operation results to log files
    
    * fix(log): simplify project path resolution

---
+++ src/services/logger.service.ts
@@ -0,0 +1,32 @@
+import fs from 'node:fs';
+import path from 'node:path';
+
+import { getProjectRootDir, resolveProjectPath } from './path.service.js';
+
+const logDir = resolveProjectPath('logs');
+
+if (!fs.existsSync(logDir)) {
+  fs.mkdirSync(logDir, {
+    recursive: true,
+  });
+}
+
+export type CMD = 'DIFF' | 'PULL' | 'PUSH';
+
+const generateLogFilePath = (opt: CMD = 'DIFF', fileName = 'default') => {
+  const timestamp = new Date().toISOString().split('T')[0];
+
+  return path.join(logDir, `${timestamp}_${opt.toLowerCase()}_${fileName}.log`);
+};
+
+// console.log(generateLogFilePath());
+
+export const writeLogLine = (opt: CMD, fileName: string, message: string) => {
+  const timestamp = new Date().toTimeString().split(' ')[0];
+
+  const logLine = `[${timestamp}] ${message}\n`;
+
+  console.log(logLine);
+
+  fs.appendFileSync(generateLogFilePath(opt, fileName), logLine, 'utf-8');
+};
